8356a4b509ca85409721b9478646b9cf8d3c9639
Rogerio <eerogerioprado@gmail.com>
2021-03-12 22:53:19 -0300

    Primeiro Projeto

--- src/index.js
+++ src/index.js
@@ -1,19 +1,42 @@
 const express = require("express");
+const {v4: uuidv4} = require("uuid");
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/courses", (request, response) =>{
-  const query = request.query;
-  console.log(query);
-  return response.json(["Curso 01", "Curso 02", "Curso 03"]);
+const customers = [];
+
+app.post("/account", (request, response) =>{
+  const {cpf, name} = request.body;
+
+  const customerAlreadyExists = customers.some( //Se tiver algum cfp igual ao cpf inserido a variavel receberá true
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists){
+    return response.status(403).json({error:"Customer already exists!"})
+  }
+  
+  const id = uuidv4();
+
+  customers.push({
+    cpf,
+    name,
+    id,
+    statement:[]
+  });
+
+
+  return response.status(201).send();
 });
 
-app.post("/courses", (request, response) =>{
-  const body = request.body;
-  console.log(body);
-  return response.json(["Curso 01", "Curso 02", "Curso 03", "Curso 04"]);
+app.get("/statement/:cpf", (request, response) =>{
+  const {cpf} = request.params;
+  
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  return response.json(customer.statement);
 });
 
 app.put("/courses/:id", (request, response) =>{
@@ -29,4 +52,5 @@ app.patch("/courses/:id", (request, response) =>{
 app.delete("/courses", (request, response) =>{
   return response.json(["Curso 01", "Curso 02", "Curso 04"]);
 });
+
 app.listen(3333);
