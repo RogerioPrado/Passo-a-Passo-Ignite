abe92bfd04613e5e97000bb7788e135f923da523
Rogerio <eerogerioprado@gmail.com>
2021-03-13 23:45:08 -0300

    Conta Bancaria

--- src/index.js
+++ src/index.js
@@ -7,6 +7,33 @@ app.use(express.json());
 
 const customers = [];
 
+//Middleware
+function verifyIfExistsAccountCPF(request, response, next){
+  const {cpf} = request.headers;
+  
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  if(!customer){
+    return response.status(400).json({error: "Customer not found!"})
+  }
+  request.customer = customer;
+
+  return next();
+}
+
+function getBalance(statement){
+const balance = statement.reduce((acc, operation)=>{//Pega as informações e calcula um resultado: acc -> Valor  adicionado/removido, operation -> identificador da operação
+  if(operation.type === 'credit'){
+    return acc + operation.amount;
+  }else{
+    return acc - operation.amount;
+  }
+
+}, 0/*svalor inicial do reduce*/);
+  console.log(balance)
+  return balance;
+};
+
 app.post("/account", (request, response) =>{
   const {cpf, name} = request.body;
 
@@ -31,26 +58,96 @@ app.post("/account", (request, response) =>{
   return response.status(201).send();
 });
 
-app.get("/statement/:cpf", (request, response) =>{
-  const {cpf} = request.params;
-  
-  const customer = customers.find(customer => customer.cpf === cpf);
+//app.use(verifyIfExistsAccountCPF);//Todas as requisições abaixo só serão feitas após a execulção do middleware
+
+app.get("/statement",verifyIfExistsAccountCPF, (request, response) =>{//somente essa requisição utilizará o middleware
+ const {customer} = request;
 
   return response.json(customer.statement);
 });
 
-app.put("/courses/:id", (request, response) =>{
-  const {id} = request.params;
-  console.log(id); //http://localhost:3333/courses?page=1&order=desc
-  return response.json(["Curso Inicial", "Curso 02", "Curso 03", "Curso 04"]);
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response)=>{
+  const {description, amount} = request.body;
+
+  const {customer} = request;
+
+  const statementOperation= {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit"
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+
 });
 
-app.patch("/courses/:id", (request, response) =>{
-  return response.json(["Curso 01", "Curso 02", "Curso 03", "Curso Final"]);
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response)=>{
+  const {amount} = request.body;
+  const {customer} = request;
+
+  const balance = getBalance(customer.statement);
+
+  if(balance < amount){
+    return response.status(400).json({error:"Insufficient funds!"});
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit"
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
 });
 
-app.delete("/courses", (request, response) =>{
-  return response.json(["Curso 01", "Curso 02", "Curso 04"]);
+app.get("/statement/date", verifyIfExistsAccountCPF, (request, response)=>{
+  const {customer} = request;
+  const {date} = request.query;
+
+  const dateFormat = new Date(date + " 00:00");
+
+  const statement = customer.statement.filter(
+    (statement) => 
+    statement.created_at.toDateString() === new Date(dateFormat).toDateString()
+  );
+
+  return response.json(statement);
 });
+
+app.put("/account", verifyIfExistsAccountCPF, (request, response) =>{
+  const { name } = request.body;
+  const { customer } = request;
+
+  customer.name = name;
+
+  return response.status(201).send();
+});
+
+app.get("/account", verifyIfExistsAccountCPF, (request, response) =>{
+  const {customer} = request;
+
+  return response.json(customer);
+});
+
+app.delete("/account", verifyIfExistsAccountCPF, (request, response)=>{
+  const {customer} = request;
+
+  customers.splice(customer, 1); //unção splice (customer é o parametro que desejo excluir, 1 é a posição que quero excluir, ou seja, quero excluir somente o parametro encontrado.)
+
+  return response.status(200).json(customers);
+});
+
+app.get("/balance", verifyIfExistsAccountCPF, (request, response) =>{
+  const {customer} = request;
+
+  const balance = getBalance(customer.statement);
+
+  return response.json(balance);
+})
 
 app.listen(3333);
